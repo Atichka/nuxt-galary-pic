89d6a60f1b95ad38becc02f1a2614482f59bedd0
Atichka <nosik_atichka@mail.ru>
2024-09-03 13:56:06 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Atichka/nuxt-galary-pic.git main:gh-pages
+git push -f https://github.com/Atichka/nuxt-galary-pic.git main:gh-pages
 
 cd -
